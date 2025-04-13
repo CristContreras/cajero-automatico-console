a7c8a2f89fc2ec58097f172945c500bd908c5983
Cristian Contreras <cristian.d.contreras@gmail.com>
2025-04-13 00:16:09 -0300

    Agrega y modifica funciones

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public static double saldoCliente = 1599.99;
-    public static Scanner teclado=new Scanner(System.in);
-    public static final String NOMBRE_CLIENTE = "Tony Stark";
-    public static final String TIPO_CUENTA = "Corriente";
-    public static byte opcionCliente;
+    private static double saldoCliente = 1599.99;
+    private static Scanner teclado = new Scanner(System.in);
+    private static final String NOMBRE_CLIENTE = "Tony Stark";
+    private static final String TIPO_CUENTA = "Corriente";
+    private static byte opcionCliente;
+    private static String entradaUsuario;
+
 
     public static void main(String[] args) {
         mostrarMenu();
     }
 
-    public static void mostrarMenu() {
+    private static void mostrarMenu() {
 
 
         String menu = """
@@ -21,27 +23,27 @@ public class Main {
                 3 - Depositar
                 4 - Salir""";
         while (true) {
-            mostrarAUsuario("\n********************************************");
-            mostrarAUsuario("Nombre del Cliente: " + NOMBRE_CLIENTE);
-            mostrarAUsuario("Tipo de cuenta: " + TIPO_CUENTA);
-            mostrarAUsuario("Saldo disponible: $" + String.format("%.2f", saldoCliente));
-            mostrarAUsuario("********************************************\n");
-            mostrarAUsuario(menu);
+            println("\n********************************************");
+            println("Nombre del Cliente: " + NOMBRE_CLIENTE);
+            println("Tipo de cuenta: " + TIPO_CUENTA);
+            println("Saldo disponible: $" + String.format("%.2f", saldoCliente));
+            println("********************************************\n");
+            println(menu);
             System.out.print("\nOpción: ");
             opcionCliente = teclado.nextByte();
 
             switch (opcionCliente) {
                 case 1:
-                    mostrarAUsuario("\n*** Opción Consultar Saldo ***\n");
-                    mostrarAUsuario(consultarSaldo());
+                    println("\n*** Opción Consultar Saldo ***\n");
+                    println(consultarSaldo());
                     preguntarSiContinuar();
                     break;
                 case 2:
-                    mostrarAUsuario("\n*** Opción Retirar Dinero ***");
+                    println("\n*** Opción Retirar Dinero ***");
                     retirarDinero();
                     break;
                 case 3:
-                    mostrarAUsuario("\n*** Opción Depositar ***");
+                    println("\n*** Opción Depositar ***");
                     depositarDinero();
                     break;
                 case 4:
@@ -54,9 +56,10 @@ public class Main {
         }
 
     }
-    public static void salirDelPrograma(){
-        mostrarAUsuario("\n********************************************");
-        mostrarAUsuario("Gracias por utilizar nuestros servicios.\nSaliendo del programa...");
+
+    private static void salirDelPrograma() {
+        println("\n********************************************");
+        println("Gracias por utilizar nuestros servicios.\nSaliendo del programa...");
         teclado.close();
         try {
             Thread.sleep(2000);
@@ -66,66 +69,112 @@ public class Main {
         System.exit(0);
     }
 
-    public static void depositarDinero() {
+    private static void depositarDinero() {
         double saldoAnterior = saldoCliente;
         double deposito = 0;
-        mostrarAUsuario("\n¿Cuanto dinero quiere depositar?");
-        System.out.print("$");
-        deposito = teclado.nextDouble();
-        saldoCliente = saldoCliente + deposito;
-        mostrarAUsuario("\nTu dinero fue depositado, cantidad: $" + deposito);
-        mostrarAUsuario("Saldo anterior: $"+saldoAnterior);
-        mostrarAUsuario(consultarSaldo());
+        do {
+            println("\n¿Cuanto dinero quiere depositar?");
+            System.out.print("$");
+            entradaUsuario = teclado.next();
+        } while (!validarNumero("double", entradaUsuario));
+        deposito = Double.parseDouble(entradaUsuario);
+        saldoCliente += deposito;
+        println("\nTu dinero fue depositado, cantidad: $" + deposito);
+        println("Saldo anterior: $" + saldoAnterior);
+        println(consultarSaldo());
+
         preguntarSiContinuar();
     }
 
-    public static void preguntarSiContinuar() {
-        boolean opcion = true;
-        while(opcion){
-            mostrarAUsuario("""
-                \n1. Volver al Menú Principal.
-                2. Salir.
-                """);
-            System.out.print("Opción: ");
+    private static boolean validarNumero(String tipoDato, String entradaUsuario) {
 
-            opcionCliente = teclado.nextByte();
-            switch (opcionCliente){
+        String mensajeError = "Error: Debe ingresar un número valido.";
+
+        if(tipoDato.equals("byte")){
+            try {
+                Byte.parseByte(entradaUsuario);
+                return true;
+            } catch (NumberFormatException e) {
+                println(mensajeError);
+                return false;
+            }
+        }else if(tipoDato.equals("int")){
+            try {
+                Integer.parseInt(entradaUsuario);
+                return true;
+            } catch (NumberFormatException e) {
+                println(mensajeError);
+                return false;
+            }
+        }else if(tipoDato.equals("double")){
+            try {
+                Double.parseDouble(entradaUsuario);
+                return true;
+            } catch (NumberFormatException e) {
+                println(mensajeError);
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+
+    private static void preguntarSiContinuar() {
+        boolean opcion = true;
+        while (opcion) {
+            println("""
+                    \n1. Volver al Menú Principal.
+                    2. Salir.
+                    """);
+
+            do {
+                System.out.print("Opción: ");
+                entradaUsuario = teclado.next();
+            } while (!validarNumero("byte", entradaUsuario));
+
+            opcionCliente = Byte.parseByte(entradaUsuario);
+
+            switch (opcionCliente) {
                 case 1:
-                    opcion=false;
+                    opcion = false;
                     break;
                 case 2:
                     salirDelPrograma();
                     break;
-                default: mostrarAUsuario("Opción incorrecta, intentelo nuevamente");
+                default:
+                    println("Error: Opción incorrecta, intentelo nuevamente");
             }
         }
     }
 
-    public static String consultarSaldo() {
-        return "Saldo disponible: $"+String.format("%.2f", saldoCliente);
+    private static String consultarSaldo() {
+        return "Saldo disponible: $" + String.format("%.2f", saldoCliente);
     }
 
-    public static void retirarDinero() {
-        double cantidadRetiro=0;
-        mostrarAUsuario(consultarSaldo());
-        mostrarAUsuario("Ingrese la cantidad de dinero a retirar");
-        System.out.print("$");
-        cantidadRetiro = teclado.nextDouble();
+    private static void retirarDinero() {
+        double cantidadRetiro = 0;
+        println(consultarSaldo());
+        do {
+            println("Ingrese la cantidad de dinero a retirar");
+            System.out.print("$");
+            entradaUsuario = teclado.next();
+        } while (!validarNumero("double", entradaUsuario));
+        cantidadRetiro = Double.parseDouble(entradaUsuario);
 
         while (cantidadRetiro > saldoCliente) {
-            mostrarAUsuario("ERROR: La cantidad solicitada excede el saldo disponible\n");
-            mostrarAUsuario("Ingrese la cantidad de dinero a retirar");
+            println("ERROR: La cantidad solicitada excede el saldo disponible\n");
+            println("Ingrese la cantidad de dinero a retirar");
             System.out.print("$");
             cantidadRetiro = teclado.nextDouble();
         }
-        mostrarAUsuario("Procesando retiro de dinero, monto: $" + cantidadRetiro);
-        saldoCliente = saldoCliente - cantidadRetiro;
-        mostrarAUsuario(consultarSaldo());
+        println("Procesando retiro de dinero, monto: $" + cantidadRetiro);
+        saldoCliente -= cantidadRetiro;
+        println(consultarSaldo());
         preguntarSiContinuar();
 
     }
 
-    public static void mostrarAUsuario(String msj) {
+    private static void println(String msj) {
         System.out.println(msj);
     }
 
